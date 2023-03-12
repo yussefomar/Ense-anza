cbb29bca8e700e7008817383c620a10b07c34def
yuscuaro <yuscuaro@gmail.com>
2023-03-12 19:13:33 -0300

    ejercicio2 programa 4.0

---
+++ Programa4.0/ejerPDF2/JS/ejercicio2.js
@@ -0,0 +1,15 @@
+ 
+let num1;
+let num2;
+let suma;
+let multiplicacion;
+let division;
+
+num1=10;
+num2=2;
+sum=num1+num2;
+division= num1/num2;
+multiplicacion=num1*num2;
+console.log(" el resultado de la multiplicacion es :",multiplicacion);
+console.log("el resultado de la division es:", division);
+console.log("el resultado de la suma es: ",sum);
