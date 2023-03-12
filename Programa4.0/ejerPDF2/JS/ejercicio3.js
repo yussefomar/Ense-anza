1924a7fecfeb14720ea203f25533fe6eeb8e2609
yuscuaro <yuscuaro@gmail.com>
2023-03-12 19:30:25 -0300

    mejoras en ejerciico pdf1 y agregado de resulto de pdf2

---
+++ Programa4.0/ejerPDF2/JS/ejercicio3.js
@@ -0,0 +1,14 @@
+ 
+let textoA;
+let textoB;
+let textoC;
+const BLANCO=" ";
+let resultado;
+
+textoA="Argentina";
+textoB="Programa";
+textoC="4.0";
+resultado= textoA+ BLANCO+textoB+ BLANCO+ textoC;
+console.log(resultado);
+ 
+ 
