dd2e40d16b3f3f9998f30f9a4ce589a1631f0a71
yussefomar <aprocalot@gmail.com>
2023-03-12 17:11:31 -0300

    nuevo curso

---
+++ Programa4.0/ejerPDF1/JS/ejercicio34pdf1.js
@@ -0,0 +1,4 @@
+ //  alert en javascript
+ /*alert() muestra un diálogo de alerta con un mensaje opcional, y aguardará hasta que el usuario cierre la ventana de diálogo */
+ alert(`hola como estas       
+   linea2 `);
